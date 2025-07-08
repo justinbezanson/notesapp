c7a07bc67124529ac86c1bbf843aaaa802ee7d2f
Justin Bezanson <justinbezanson@gmail.com>
2025-07-08 20:29:32 -0300

    setup seeders, note resources

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->title(),
+            'content' => $this->faker->paragraph(),
+            'slug' => $this->faker->unique()->slug(),
+            'user_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
